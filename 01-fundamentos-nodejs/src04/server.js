d4a59ec7b04a5aeb4109bb1a2b277a25ac76f97d
Gierre Jr <gierremartins@gmail.com>
2023-08-22 13:44:58 -0300

    - with '#' will be ignored, and an empty message aborts the commit. -# Changes to be committed:

--- 01-fundamentos-nodejs/src04/server.js
+++ 01-fundamentos-nodejs/src04/server.js
@@ -7,7 +7,6 @@ import { extractQueryParams } from './utils/extract-query-params.js';
 const server = http.createServer(async(req, res) => { 
 
   const { method, url} = req 
-
   await json(req, res) 
 
   const route = routes.find(route => {
